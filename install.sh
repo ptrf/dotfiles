dd36f27c2ca2e44e40acfd61cc668bca344bf15f
Peter Tersløv Forsberg <ptrf@pronoia.dk>
2015-08-15 18:43:04 +0200

    Added new install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
 
-[ -z $TARGET_DEST ] && TARGET_DEST=~
+# optional pulling
+[ ! -z "$PERFORM_GIT_PULL" ] && git pull origin master
 
-function doIt() {
-    rsync -avhL --exclude ".git/" --exclude ".DS_Store" --no-perms targets/$(uname -s)/ $TARGET_DEST
-	source $TARGET_DEST/.bash_profile
+# ensure target
+[ -z "$TARGET_DEST" ] && echo "WARNING: TARGET_DEST unset, staging for ~" && TARGET_DEST=~
+
+# ensure system
+[ -z "$TARGET_SYSTEM" ] && TARGET_SYSTEM=$(uname -s)
+
+
+function install_to_target() {
+    rsync -avhL --exclude ".git/" --exclude ".gitkeep" --exclude ".DS_Store" --no-perms targets/$TARGET_SYSTEM/ $TARGET_DEST
+    [ -x "hooks/${TARGET_SYSTEM}/runhook.sh" ] && echo "There was a run hook script!"
+    source $TARGET_DEST/.bash_profile
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+    install_to_target
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        install_to_target
+    fi
 fi
-unset doIt
+unset install_to_target
