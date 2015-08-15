a86421b38d96a608660a4197e820cf88d249ec97
Peter Tersløv Forsberg <ptrf@pronoia.dk>
2015-08-15 19:13:32 +0200

    Updated+renamed brew stuff (brew.sh brewcask.sh)

---
+++ brewcask.sh
@@ -0,0 +1,29 @@
+# Install native apps
+# Usage: `brew bundle Caskfile`
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+brew cask install google-chrome 2> /dev/null
+brew cask install google-chrome-canary 2> /dev/null
+brew cask install chromium 2> /dev/null
+brew cask install iterm2 2> /dev/null
+brew cask install macvim 2> /dev/null
+brew cask install opera 2> /dev/null
+brew cask install opera-developer 2> /dev/null
+brew cask install opera-next 2> /dev/null
+brew cask install virtualbox 2> /dev/null
+brew cask install vlc 2> /dev/null
+brew cask install slate 2> /dev/null
+brew cask install vagrant 2> /dev/null
+brew cask install skim 2> /dev/null
+brew cask install dash 2> /dev/null
+brew cask install mplayerx 2> /dev/null
+brew cask install emacs 2> /dev/null
+brew cask install caffeine 2> /dev/null
+brew cask install firefox 2> /dev/null
+brew cask install silverlight 2> /dev/null
+brew cask install spotify 2> /dev/null
+brew cask install soundcleod 2> /dev/null
+brew cask install qgis 2> /dev/null
+brew cask install mactex 2> /dev/null
